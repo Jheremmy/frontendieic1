b7f4cd3e57826cf4f098046728d3e9660f076a43
Jheremmy <168200028+Jheremmy@users.noreply.github.com>
2024-06-25 14:51:10 -0400

    CALSE 2

---
+++ src/Componentes/Pagina1.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Pagina1() {
+  return (
+    <>
+      <h1>Bienvenido</h1>
+      <p>Te saludo desde la pagina1</p>
+    </>
+  )
+}
+export default Pagina1
---
+++ src/Componentes/Perfil.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { Permisos } from './Permisos'
+interface Props{
+    nombre:string,
+    apellido:string,
+    edad?:number
+}
+const Perfil = (props:Props) => {
+  return (
+    <>
+    <h1>Hola!</h1>
+    <p>Nombre: {props.nombre}</p>
+    <p>Apellido: {props.apellido}</p>
+    <p>Edad: {props.edad}</p>
+    {
+        <>
+        props.edad!=undefined &&
+        props.edad > 18 &&
+        <p>Tienes permitido pasar</p>
+        <Permisos permiso1='Beber' permiso2='Conducir'
+        </>
+    }
+    
+    {
+        <>
+        props.edad!=undefined &&
+        props.edad > 18 &&
+        <p>Tienes permitido pasar</p>
+        <Permisos permiso1='Tomar leche' permiso2='Divertirse'
+        </>
+    }
+    </>
+  )
+}
+
+export default Perfil
